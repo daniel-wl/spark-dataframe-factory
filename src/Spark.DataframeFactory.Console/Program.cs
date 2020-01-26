c668556fae04db7d8c8c8eb98cbb11d9c971c6a1
Daniel Long <daniel0long@gmail.com>
2020-01-26 17:03:33 -0500

    First test of .net spark driver

---
+++ src/Spark.DataframeFactory.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Spark.DataframeFactory.Core;
+
+namespace Spark.DataframeFactory.Console {
+    class Program {
+        static void Main (string[] args) {
+            SparkTest.RunSpark();
+            System.Console.ReadLine();
+        }
+    }
+}
