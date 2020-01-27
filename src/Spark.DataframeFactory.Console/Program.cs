cc981c6bceefa701230ea7c8aa43da6da880312e
Daniel Long <daniel0long@gmail.com>
2020-01-26 20:18:47 -0500

    Add factory class

--- src/Spark.DataframeFactory.Console/Program.cs
+++ src/Spark.DataframeFactory.Console/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Microsoft.Spark.Sql;
 using Spark.DataframeFactory.Core;
 
-namespace Spark.DataframeFactory.Console {
-    class Program {
-        static void Main (string[] args) {
-            SparkTest.RunSpark();
+namespace Spark.DataframeFactory.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SparkSession spark = SparkSession.Builder().GetOrCreate();
+            var factory = new Core.DataframeFactory(spark);
+            factory.Build();
             System.Console.ReadLine();
         }
     }
---
+++ src/Spark.DataframeFactory.Core/DataframeFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Spark.Sql;
+using Microsoft.Spark.Sql.Types;
+
+namespace Spark.DataframeFactory.Core
+{
+    public class DataframeFactory
+    {
+        internal SparkSession Spark { get; set; }
+
+        public DataframeFactory(SparkSession spark)
+        {
+            if (spark == null)
+            {
+                throw new ArgumentNullException(nameof(spark));
+            }
+
+            Spark = spark;
+        }
+
+        public DataFrame Build()
+        {
+            return Build(
+                new [] { new GenericRow(new object[] { }) },
+                new Microsoft.Spark.Sql.Types.StructType(new StructField[] { }));
+        }
+
+        public DataFrame Build(IEnumerable<GenericRow> data, StructType schema)
+        {
+            return Spark.CreateDataFrame(data, schema);
+        }
+    }
+}
